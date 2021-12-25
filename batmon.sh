97fd3512c97b47ec7e9adcdd8adfc10ddf52581c
aomanchuria <aomanchuria@gmail.com>
2021-12-25 09:40:53 -0800

    Update batmon.sh

--- batmon.sh
+++ batmon.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-/bin/sleep 60 && /usr/bin/python3 /home/pi/Documents/PID_finder.py
+#this script waits for 60 seconds, then starts the SeniorPrius script
+#use this as a cron job at boot time
+/bin/sleep 60 && /usr/bin/python3 /home/pi/Documents/SeniorPrius_V0001.py
